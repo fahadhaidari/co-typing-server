27e789688b701403b6450a43a8a60f9c222978d3
Fahad Haidari <fahad.haidari@gmail.com>
2017-08-11 15:32:32 -0600

    server is listening on port 8080 and index.js inside socket folder does export a function with io as a parameter

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const sockets = require("./socket");
+const sockets = require("./socket/index.js");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const io = requrie("socket.io")(server);
+const io = require("socket.io")(server);
 
 
 sockets(io);
@@ -13,6 +13,11 @@ app.use(function(req, res, next) {
   var err = new Error("Not found");
   err.status = 404;
   next(err);
+});
+
+
+server.listen(8080, function() {
+  console.log("Listening on PORT:8080");
 });
 
 module.exports = {
