fb89cf5424fd1ce4ddace41b498ed6968798feb7
Fahad Haidari <fahad.haidari@gmail.com>
2017-08-16 00:37:58 -0600

    server will get full text from the client and then resend it to other users in the same room/session

--- socket/index.js
+++ socket/index.js
@@ -8,7 +8,7 @@ module.exports = function(io) {
 
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-      db.collection("room").find({}).toArray(function(err, result) {
+    db.collection("room").find({}).toArray(function(err, result) {
       if (err) throw err;
       rooms = result;
       db.close();
@@ -25,19 +25,18 @@ module.exports = function(io) {
       let room = findRoom(_info.roomName);
 
       console.log("Server info ", _info)
-      if(room) {
+      if (room) {
 
-        console.log("Room does exist " , room.name);
+        console.log("Room does exist ", room.name);
 
         let userNameIndex = room.users.indexOf(_info.userName);
-        if(userNameIndex < 0) {
+        if (userNameIndex < 0) {
           room.users.push(_info.userName);
           socket.join(_info.roomName);
-          console.log("User " , _info.userName, " joined " , _info.roomName);
-          console.log("Room users " , room.users);
+          console.log("User ", _info.userName, " joined ", _info.roomName);
+          console.log("Room users ", room.users);
           socket.emit('success', _info.userName);
           socket.broadcast.to(_info.roomName).emit('joined', _info.userName);
-
         } else {
           console.log("User does exist");
         }
@@ -49,14 +48,13 @@ module.exports = function(io) {
     });
 
     socket.on("message", function(_info) {
-      console.log("Data coming form the client " , _info);
+      console.log("Data coming form the client ", _info);
       socket.broadcast.to(_info.roomName).emit('message', _info);
     });
 
-
     function findRoom(_roomName) {
       for (var i = 0; i < rooms.length; i++) {
-        if(rooms[i].name == _roomName) {
+        if (rooms[i].name == _roomName) {
           return rooms[i];
           break;
         }
